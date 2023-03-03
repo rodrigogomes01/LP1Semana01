59a3d7dde0a6caeae5220a3915b118b2b85a2858
rodrigogomes01 <115220374+rodrigogomes01@users.noreply.github.com>
2023-03-03 19:09:20 +0000

    Print if it has more than 4 characters

---
+++ ArgLongStrings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ArgLongStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i].Length > 4)
+                    Console.WriteLine(args[i].ToUpper());
+            }
+        }
+    }
+
+}
