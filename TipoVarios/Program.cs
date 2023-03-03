1cc3694e5a95d7cd948061d417a8514593a834b1
rodrigogomes01 <115220374+rodrigogomes01@users.noreply.github.com>
2023-03-03 17:29:23 +0000

    Create variables

---
+++ TipoVarios/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TipoVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Integral variables
+            byte number1 = 12;
+            sbyte number2 = -2;
+            int number3 = 84387;
+
+            // Print the integral variables
+            Console.WriteLine(number1);
+            Console.WriteLine(number2);
+            Console.WriteLine(number3);
+        }
+    }
+}
