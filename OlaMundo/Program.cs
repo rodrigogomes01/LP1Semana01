67f861097a0f2222b7ac115bdfe6a7a92bc2fc2b
rodrigogomes01 <115220374+rodrigogomes01@users.noreply.github.com>
2023-03-03 17:18:52 +0000

    Added XML comments

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -2,8 +2,15 @@
 
 namespace OlaMundo
 {
+    /// <summary>
+    /// 
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
