07c1ad2fc1f9539e8eb6f17a70ca12ccb2cd1351
rodrigogomes01 <115220374+rodrigogomes01@users.noreply.github.com>
2023-03-03 14:44:49 +0000

    First commit

---
+++ OlaMundo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Bye World!")
+        }
+    }
+}
