df90a0bbf46cc49723d78ddea1d1e1c86c935075
rodrigogomes01 <115220374+rodrigogomes01@users.noreply.github.com>
2023-03-03 18:04:06 +0000

    Added variable strings

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /// String variables
+            string normal = "\tThis is a normal string\n";
+            string verbatim = @"This is a more special string""That allows this"" To be more special";
+
+            ///Print the string variables
+            Console.WriteLine(normal);
+            Console.WriteLine(verbatim);
+        }
+    }
+}
